9034ff2c0c26d96bf5a6d0c5d486fc7b06462573
GuilhermeErthal <guilhermeerthal@hotmail.com>
2020-09-27 12:00:39 -0300

    createFunctionMapReduce

--- apps/cinnecta/models.py
+++ apps/cinnecta/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.signals import pre_save
 
 
 class Base(models.Model):
--- apps/vectorwords/vectors.py
+++ apps/vectorwords/vectors.py
@@ -1,9 +1,10 @@
 
-from stopwords import allStopWords
+#Importação do minceamet disponibilizado no link: https://github.com/michaelfairley/mincemeatpy
+import mincemeat
 import glob
 import csv
 
-
+#Realiza o agrupamento dos arquivos adicionados pela API
 text_files = glob.glob('C:\\Users\\hac027\\Documents\\GitHub\\WordsVector\\media\\*')
 
 def file_contents(file_name):
@@ -17,6 +18,7 @@ source = dict((file_name, file_contents(file_name)) for file_name in text_files)
 
 def mapfn(k, v):
 	print('map ' + k)
+	from stopwords import allStopWords
 	for line in v.splitlines():
 		for word in line.split():
 			if (word not in allStopWords ):
@@ -26,7 +28,9 @@ def reducefn(k, v):
 	print('reduce ' + k)
 	return sum(v)
 
+
 s = mincemeat.Server()
+
 
 #A fonte de dados pode ser qualquer objeto do tipo dicionário
 s.datasource = source
@@ -35,9 +39,8 @@ s.reducefn = reducefn
 
 results = s.run_server(password="changeme")
 
-mapfn()
-reducefn()
 
-w = csv.writer(open("/media/RESULT.csv", "w"))
+#Cria o documento com o resultado das palavras
+w = csv.writer(open("C:\\Users\\hac027\\Documents\\GitHub\\WordsVector\\media\\RESULT.csv", "w"))
 for k, v in results.items():
 	w.writerow([k, v])
